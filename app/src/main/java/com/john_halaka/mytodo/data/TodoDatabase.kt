4e4f22c896b508379637d33d9d2a3e20bdc81bac
John S. Halaka <94745774+JohnSobhy@users.noreply.github.com>
2023-04-24 12:57:23 +0200

    di with daggerHilt

--- app/src/main/java/com/john_halaka/mytodo/data/TodoDatabase.kt
+++ app/src/main/java/com/john_halaka/mytodo/data/TodoDatabase.kt
@@ -1,8 +1,6 @@
 package com.john_halaka.mytodo.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
@@ -15,25 +13,5 @@ abstract class TodoDatabase : RoomDatabase() {
 
     abstract val todoDao: TodoDao
 
-    companion object {
-        @Volatile
-        private var INSTANCE: TodoDatabase? = null
-        fun getDatabase(context: Context): TodoDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    TodoDatabase::class.java,
-                    "todo-db"
-                ) .build()
-                INSTANCE = instance
-                return instance
-            }
 
-
-        }
     }
-}
