77a25306ae77881a755bffbc84bc63e87da3ee0f
John S. Halaka <94745774+JohnSobhy@users.noreply.github.com>
2023-04-27 13:23:34 +0200

    AddEditTodo viewModel

--- app/src/main/java/com/john_halaka/mytodo/data/Todo.kt
+++ app/src/main/java/com/john_halaka/mytodo/data/Todo.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity (tableName = "todo_table")
 data class Todo (
    @PrimaryKey (autoGenerate = true)
-   val id: Int,
+   val id: Int?,
     val title: String,
     val description: String?,
     val completed: Boolean,
